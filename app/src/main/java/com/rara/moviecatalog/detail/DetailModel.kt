dcc5c6d9dd398562c1851896ff0648b874e7f456
Rara Widya <rara12148@gmail.com>
2020-10-04 16:15:59 +0700

    Update Detail

--- app/src/main/java/com/rara/moviecatalog/detail/DetailModel.kt
+++ app/src/main/java/com/rara/moviecatalog/detail/DetailModel.kt
@@ -4,12 +4,16 @@ data class MovieDetailModel(
     val title: String,
     val poster_path: String,
     val overview: String,
-    val backdrop_path: String
+    val backdrop_path: String,
+    val release_date: String,
+    val runtime: String
 )
 
 data class TvShowDetailModel(
     val name: String,
     val overview: String,
     val poster_path: String,
-    val backdrop_path: String
+    val backdrop_path: String,
+    val first_air_date : String,
+    val episode_run_time: List<Int>
 )
